a0a04ae993b396901d9c1871a80a351f4b6829a3
darksoul333666 <darkdevil333666@gmail.com>
2023-02-12 16:19:12 -0600

    router

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
       element:<Navbar/>,
       children: [
         {
-          path: "home",
+          path: "/",
           element: <HomeScreen/>,
         },
         {
