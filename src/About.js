d25e7f7ca01107c850f7f538d7d66167b9754092
darksoul333666 <darkdevil333666@gmail.com>
2023-02-12 16:20:01 -0600

    fix margin

--- src/About.js
+++ src/About.js
@@ -13,7 +13,7 @@ import Box from '@mui/material/Box';
           <h2 style={{flex:7}} > Acerca de Nosotros</h2>
         </div>
         <div style={{flex:8, backgroundColor:'white', flexDirection:'column'}} >
-        <div style={{flex:1, width:"95%", margin:'auto', margin:'2.5%'  }} >
+        <div style={{flex:1, width:"95%",  margin:'2.5%'  }} >
         <img style={{width:"100%"}} src={require('./assets/about.jpeg')}/>
         <img style={{width:"100%"}} src={require('./assets/mision.jpeg')}/>
         </div>
